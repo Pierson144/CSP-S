42ac153072ccb89b8919cb84650c7d65bc8a29ab
Pierson <palexander3629@floridapoly.edu>
2023-11-20 16:50:47 -0500

    Changed frequency in sine wave plot for testing

---
+++ sineWave.py
@@ -0,0 +1,17 @@
+# sine_wave.py
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_sine_wave(frequency=2, duration=1, sample_rate=1000):
+    time = np.arange(0, duration, 1/sample_rate)
+    amplitude = np.sin(2 * np.pi * frequency * time)
+
+    plt.plot(time, amplitude)
+    plt.title(f"Sine Wave - Frequency: {frequency} Hz")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    plot_sine_wave()
